b171742f8c8f0d7d0d59e8758e7a7836b354ee3c
Strider <mezentsev.k@gmail.com>
2023-10-19 22:44:59 +0800

    Добавлен путь приветствия

--- src/main/java/com/calculator/calculator/CalcContr.java
+++ src/main/java/com/calculator/calculator/CalcContr.java
@@ -1,6 +1,7 @@
 package com.calculator.calculator;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,11 @@ public class CalcContr {
     @GetMapping(path = "/calculator")
     public String wellcome(){
         return "Добро пожаловать";
+    }
+    public int num1;
+    public int num2;
+    @GetMapping(path = "/calculator/plus")
+    public int plus(int num1, int num2) {
+         return num1+num2;
     }
 }
